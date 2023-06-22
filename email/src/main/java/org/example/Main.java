0a26bc3a8e84bf30577fdb503266bbffb4092bdf
Ceco <c_vatovski@abv.bg>
2023-06-22 11:29:24 +0300

    mess

--- email/src/main/java/org/example/Main.java
+++ email/src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
+import entities.User;
+import orm.EntityManager;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static orm.MyConnector.createConnection;
+import static orm.MyConnector.getConnection;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+        createConnection("root", "nPcFu9W^DmCyDP*f", "custom_orm");
+        Connection connection = getConnection();
+        EntityManager<User> userEntityManager = new EntityManager<>(connection  );
     }
 }
